8086cb0c8e7f883b8520b2e2934c6ead044b91bd
Seren-Hughes <seren.hughes@gmail.com>
2025-06-13 22:54:09 +0100

    add: add Procfile for deployment and set debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*l_5w29hla+%7f-qj4+g$#chhzd()(n_%+)w5edpl_wtm@ntqj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',  # Allow all subdomains of herokuapp.com
+]
 
 
 # Application definition
