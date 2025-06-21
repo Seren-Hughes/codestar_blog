06825a318884980d080f2a735acc15fbcbd3f4e6
Seren-Hughes <seren.hughes@gmail.com>
2025-06-21 18:31:52 +0100

    add: implement CollaborateRequest model and form, enhance about view and template for collaboration feature

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_view(request):
@@ -7,4 +8,9 @@ def about_view(request):
     Render the about page with the latest About object.
     """
     about = About.objects.all().order_by('-updated_on').first()
-    return render(request, 'about/about.html', {'about': about})
+    collaborate_form = CollaborateForm()
+    
+    return render(request, 'about/about.html', {
+            'about': about,
+            'collaborate_form': collaborate_form
+        })
